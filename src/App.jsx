4ba7b4d5a62d6031e1cba59a91879849848fbef9
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-05-08 00:15:53 +0300

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Forecast from './components/Forecast.jsx'
 
 function App() {
+
+  let [data , setData] = useState()
+
+  let API_KEY = "08dead54eae17a7e45d013b4a4358b59"
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=&appid=${API_KEY}`
+
   return (
     <>
      <Forecast  title= "HOURLY FORECAST"/>
