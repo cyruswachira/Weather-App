6c5408b730687c9fc6a17f0084f7ea0be04b4dcc
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-05-08 01:21:43 +0300

    working with the data

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import Forecast from './components/Forecast.jsx'
 
 function App() {
 
-  let [data , setData] = useState()
+  let [data , setData] = useState({})
+
+  let [location , setLocation] = useState("")
 
   let API_KEY = "08dead54eae17a7e45d013b4a4358b59"
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=&appid=${API_KEY}`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+   
 
+  function searchLocation(){}
   return (
     <>
-     <Forecast  title= "HOURLY FORECAST"/>
-
-     <Forecast  title= "DAILY FORECAST"/>
+      <div className='w-full h-full relative'>
+     
+     <div className='text-center p-4'>
+      <input type="text"  className='py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-100 text-black focus:outline-none' placeholder='Enter Location' value = {location} onChange={(e) => setLocation(e.target.value)} onKeyDownCapture={searchLocation}/>
+     </div>
+               
+      </div>
 
     </>
   )
