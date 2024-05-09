ef1884ebbb095f193a41e5102ee332d6a9581c08
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-05-09 10:42:12 +0300

    useDate done

--- src/Utils/useDate.jsx
+++ src/Utils/useDate.jsx
@@ -1,4 +1,30 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 
 
-export let useDate = () => {}
+export let useDate = () => {
+
+    let locale = 'en'
+
+    let [today , setDate] = useState(new Date())
+
+    useEffect(() => {
+        let timer = setInterval(() =>{
+            setDate(new Date())
+        }, 60*1000)
+        return function cleanup() {
+            clearInterval(timer)
+        }
+        
+    },[])
+
+    let day = today.toLocaleDateString(locale , {weekday : 'long'})
+
+    let date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale , {month : 'long'})}\n\n`
+
+     let time = today.toLocaleDateString(locale , {hour : 'numeric' , hour12 : true , minute : 'numeric'})
+
+
+     return(date , time)
+
+
+}
