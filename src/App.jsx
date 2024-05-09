efc5aa65b2595149974990c6f77cc5f8fc971902
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-05-09 22:04:15 +0300

    background

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Search from "../src/components/Search";
 import Header from "./components/Header";
 import WeatherMap from "./components/WeatherMap";
 import CurrentWeather from "./components/CurrentWeather";
-
+import Background from "./components/BackGround";
 function App() {
   const handleOnSearchChange = (searchData) => {
     console.log(searchData);
   };
   return (
     <>
+      <div className="Background">
+        <Background />
+      </div>
       <div className="weather-app">
         <Header />
-        <CurrentWeather/>
-        <WeatherMap/>
+        <CurrentWeather />
+        <WeatherMap />
       </div>
     </>
   );
