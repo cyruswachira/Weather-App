98a1e9c54f45dc91d35d0125f088d38cd9c33f1f
Leslie Mwangi <luciannahphoi@gmail.com>
2024-05-13 10:00:17 +0300

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-center pt-2 text-5xl font-serif">
-        <h1>WEATHER APP</h1>
+      <div className="flex justify-center pt-2 text-5xl font-serif font-bold">
+        <h1><u>WEATHER APP</u></h1>
       </div>
 
-      <div className="pl-2 flex flex-row">
+      <div className="pl-2 flex flex-row font-bold mt-5 mb-5">
         <nav className="flex gap-2">
           <div className="mr-4">
             <Link to="/" className="text-black text-black hover:text-white">
               Current Weather
             </Link>
           </div>
-          <div>
+          <div className="mr-5">
             <Link
               to="/weather-map"
               className="text-black text-black hover:text-white"
